479c000efa40b482abe269ff4f73d3694da7891e
anna-gorska <aniajj9@gmail.com>
2023-09-11 14:54:22 +0200

    adding next steps of the proticol

--- One time truth table Assignment/main.py
+++ One time truth table Assignment/main.py
@@ -23,20 +23,41 @@ blood_type_to_index = {
     'AB+': 7
 }
 
+n = 8
+
+
 def circular_shift_matrix(matrix, r, s):
     num_rows = len(matrix)
     num_cols = len(matrix[0])
-    
+
     shifted_matrix = [[0 for _ in range(num_cols)] for _ in range(num_rows)]
-    
+
     for i in range(num_rows):
         for j in range(num_cols):
             shifted_matrix[(i + r) % num_rows][(j + s) % num_cols] = matrix[i][j]
-    
+
     return shifted_matrix
+
 
 def generate_random_matrix(n):
     return [[random.choice([True, False]) for _ in range(n)] for _ in range(n)]
+
+
+def two_dimensions_xor(array1, array2):
+    result = []
+    # Iterate through the rows of the arrays
+    for i in range(len(array1)):
+        # Create an empty row for the result
+        result_row = []
+        # Iterate through the columns of the arrays
+        for j in range(len(array1[i])):
+            # Perform XOR operation on corresponding elements
+            result_element = array1[i][j] ^ array2[i][j]
+            # Append the result to the current row
+            result_row.append(result_element)
+        # Append the row to the result array
+        result.append(result_row)
+    return result
 
 
 class Alice:
@@ -44,24 +65,70 @@ class Alice:
         self.__r = None
         self.__Ma = None
         self.__x = x
-    
+        self.__V = None
+        self.__U = None
+        self.__zb = None
+        self.__z = None
+
     def set_r(self, r):
         self.__r = r
-    
+
     def set_Ma(self, Ma):
         self.__Ma = Ma
 
+    def set_V(self, V):
+        self.__V = V
+
+    def compute_U(self):
+        assert self.__x is not None and self.__r is not None
+        self.__U = (self.__x + self.__r) % (2**n)
+
+    def send_U_to_Bob(self, bob):
+        bob.set_U(self.__U)
+
+    def set_zb(self, zb):
+        self.__zb = zb
+
+    def calculate_z(self):
+        self.__z = self.__zb ^ self.__Ma[self.__U][self.__V]
+
+    def get_z(self):
+        return self.__z
+
+
 class Bob:
     def __init__(self, y):
-        self.s = None
-        self.Mb = None
-        self.y = y
-    
+        self.__s = None
+        self.__Mb = None
+        self.__y = y
+        self.__V = None
+        self.__U = None
+        self.__zb = None
+
     def set_s(self, s):
-        self.s = s
-    
+        self.__s = s
+
     def set_Mb(self, Mb):
-        self.Mb = Mb
+        self.__Mb = Mb
+
+    def set_U(self, U):
+        self.__U = U
+
+    def compute_V(self):
+        assert self.__y is not None and self.__s is not None
+        self.__V = (self.__y + self.__s) % (2 ** n)
+
+    def send_V_to_Alice(self, alice):
+        alice.set_V(self.__V)
+
+    def calculate_zb(self):
+        assert self.__Mb is not None and self.__U is not None and self.__V is not None
+        self.__zb = self.__Mb[self.__U][self.__V]
+
+    def send_zb_to_Alice(self, alice):
+        alice.set_zb(self.__zb)
+
+
 
 class Dealer:
     def __init__(self):
@@ -69,22 +136,61 @@ class Dealer:
         self.__s = None
         self.__Mb = None
         self.__Ma = None
-    
+        self.__zb = None
+
     def pick_random_r(self):
-        self.__r = random.randint(0, 8)
-    
+        self.__r = random.randint(0, n)
+
     def pick_random_s(self):
-        self.__s = random.randint(0, 8)
-    
+        self.__s = random.randint(0, n)
+
     def pick_random_Mb(self):
-        self.__Mb = generate_random_matrix(8)
-    
+        self.__Mb = generate_random_matrix(n)
+
     def calculate_ma(self):
         shifted_table = circular_shift_matrix(self.__Mb, self.__r, self.__s)
-        self.__Ma = 
+        self.__Ma = two_dimensions_xor(shifted_table, self.__Mb)
+
+    def set_Alice_values(self, alice):
+        assert self.__r is not None and self.__Ma is not None
+        alice.set_r(self.__r)
+        alice.set_Ma(self.__Ma)
+
+    def set_Bob_values(self, bob):
+        assert self.__s is not None and self.__Mb is not None
+        bob.set_s(self.__s)
+        bob.set_Mb(self.__Mb)
 
 
-    
-    
+def execute_protocol(x, y):
+    dealer = Dealer()
+    alice = Alice(x)
+    bob = Bob(y)
+
+    dealer.pick_random_s()
+    dealer.pick_random_r()
+    dealer.pick_random_Mb()
+    dealer.calculate_ma()
+    dealer.set_Alice_values(alice)
+    dealer.set_Bob_values(bob)
+
+    # Exchange secrets
+    alice.compute_U()
+    bob.compute_V()
+    alice.send_U_to_Bob(bob)
+    bob.send_V_to_Alice(alice)
+    bob.calculate_zb()
+    bob.send_zb_to_Alice(alice)
+
+    # Calculate z
+    alice.calculate_z()
+    print("Result value z is: ", alice.get_z())
+
+
+execute_protocol(1, 0)
+
+
+
+
 
 
