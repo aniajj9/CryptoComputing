050287fa9262830988f2738f3d10a3c326b874c2
anna-gorska <aniajj9@gmail.com>
2023-09-14 18:56:28 +0200

    working blood

--- 3 Bedoza/main.py
+++ 3 Bedoza/main.py
@@ -137,22 +137,6 @@ class Dealer:
         bob.set_and_wire_values("W_b", W_b)
 
 
-'''xa, xb, xr, ya, yb, yr = 0, 0, 0, 0, 0, 0
-dealer = Dealer(1)
-part_1 = dealer.xor_constant(1, xa)
-part_2 = dealer.and_wires(part_1, ya)
-part_3 = dealer.xor_constant(1, part_2)
-part_4 = dealer.xor_constant(1, xb)
-part_5 = dealer.and_wires(part_4, yb)
-part_6 = dealer.xor_constant(1, part_5)
-part_7 = dealer.and_wires(part_3, part_6)
-part_8 = dealer.xor_constant(1, xr)
-part_9 = dealer.and_wires(part_8, yr)
-part_10 = dealer.xor_constant(1, part_9)
-part_11 = dealer.and_wires(part_7, part_10)
-
-result = part_11'''
-
 blood_types = {
     '0-': "000",
     '0+': "001",
